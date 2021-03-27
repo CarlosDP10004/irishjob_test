09046cfb3681ec48f0846382653716617de3e939
cdiaztester <carlos.diaz.dp10004@gmail.com>
2021-03-27 12:35:46 -0600

    Se agrega fix en clase Test_Case_01 para utilizar el Assert-assertTrue y verificar que el valor devuelto sea mayor que cero

--- irishjob_test/src/test/java/Test_Case_01.java
+++ irishjob_test/src/test/java/Test_Case_01.java
@@ -1,5 +1,6 @@
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,19 +13,6 @@ public class Test_Case_01 {
     private Methods FW = new Methods();
     private WebDriver webDriver = Driver.webDriver;
 
-    /*@Step("Go to site <url> now")
-    public void go_to_site(String url){
-        FW.open_browser(url);
-    }
-
-    @Step("Login user <user> with password <password>")
-    public void login_user(String user, String password){
-        FW._javascript_element(webDriver, "/html[1]/body[1]/span[1]/span[1]/div[2]/div[1]/div[2]/button[2]");
-        FW._get_element_by_id(webDriver, "sign-in-toggle").click();
-        FW._get_element_by_id(webDriver, "Email").sendKeys("tester.cdiaz@gmail.com");
-        FW._get_element_by_id(webDriver, "Password").sendKeys("20.Serv3r.Id");
-        FW._get_element_by_id(webDriver, "loginButton").click();
-    }*/
     @Step("Verify amount jobs recommended for you")
     public void verify_jobs(){
         FW._get_element_by_xpath(webDriver, "//a[contains(text(),'IrishJobs.ie')]").click();
@@ -32,8 +20,8 @@ public class Test_Case_01 {
         List<WebElement> elements = element.findElements(By.tagName("div"));
         System.out.print("Amount of jobs found is: " + elements.size());
         elements.forEach(System.out::println);
-
-        assertThat(elements).isNotNull();
+        Assert.assertTrue(elements.size() > 0);
+        //assertThat(elements).isNotNull();
     }
 
 }
