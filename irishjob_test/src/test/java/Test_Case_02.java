2a28a4522a33e48595c443bdce2d2bdd5fbc578f
cdiaztester <carlos.diaz.dp10004@gmail.com>
2021-03-27 12:21:42 -0600

    Se agrega fix en las clases que describen los pasos de la ATDD

--- irishjob_test/src/test/java/Test_Case_02.java
+++ irishjob_test/src/test/java/Test_Case_02.java
@@ -1,7 +1,11 @@
 import com.thoughtworks.gauge.Step;
 import driver.Driver;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+
+import java.util.Locale;
+
 import static org.junit.Assert.assertTrue;
 
 public class Test_Case_02 {
@@ -16,17 +20,23 @@ public class Test_Case_02 {
         FW._get_element_by_xpath(webDriver, "//input[@id='Keywords']").sendKeys(job);
         FW._get_element_by_id(webDriver, "btnSubmit").click();
         String value = FW._get_element_by_xpath(webDriver, "//div[contains(text(),'No jobs were found to match your criteria, you can')]").getText();
-        assertTrue(value.contains("No jobs were found to match your criteria, you can"));
+        System.out.print("Value is: " + value);
+        value = value.toUpperCase();
+        Assert.assertTrue(value.contains(("No jobs were found to match your criteria, you can").toUpperCase()));
     }
 
     @Step("Search a real job <job>")
     public void search_real_job(String job){
         FW._get_element_by_xpath(webDriver, "//a[contains(text(),'IrishJobs.ie')]").click();
+        FW._get_element_by_xpath(webDriver, "//span[contains(text(),'×')]").click();
         FW._get_element_by_xpath(webDriver, "//input[@id='Keywords']").clear();
+
         FW._get_element_by_xpath(webDriver, "//input[@id='Keywords']").sendKeys(job);
         FW._get_element_by_id(webDriver, "btnSubmit").click();
         String value = FW._get_element_by_xpath(webDriver, "//label[contains(text(),'Total Jobs Found:')]").getText();
-        assertTrue(value.contains("Total Jobs Found:"));
+        System.out.print("Value is: " + value);
+        value = value.toUpperCase();
+        Assert.assertTrue(value.contains(("TOTAL JOBS FOUND:").toUpperCase()));
     }
 
 }
